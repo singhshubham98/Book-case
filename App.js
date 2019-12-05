1a94fab7863b46773c4658243f0870a5e6a7c530
singhshubham98 <shubhamsingh4204@gmail.com>
2019-12-05 08:44:58 +0530

    Bottom tab navigator setup :tada:

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import Router from "./components/Router";
+import { View } from "react-native";
+import RootStack from "./components/Router";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Router />
+    <View>
+      <RootStack />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
--- components/Router.js
+++ components/Router.js
@@ -1,13 +1,50 @@
 import React from "react";
-import { Dimensions, Platform } from "react-native";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
+import { Icon } from "react-native-elements";
+
 import Bookcase from "./Bookcase";
+import Explore from "./Explore";
+import AddBook from "./AddBook";
+import Lists from "./Lists";
+import Profile from "./Profile";
 
-const createRootNavigator = createStackNavigator({
-  Home: {
-    screen: Bookcase
+const TabNavigator = createBottomTabNavigator(
+  {
+    Bookcase: Bookcase,
+    Explore: Explore,
+    "Add Book": AddBook,
+    Lists: Lists,
+    "My Profile": Profile
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName, iconType;
+        if (routeName === "Bookcase") {
+          (iconName = `open-book`), (iconType = "entypo");
+        } else if (routeName === "Explore") {
+          (iconName = "ios-map"), (iconType = "ionicon");
+        } else if (routeName === "Add Book") {
+          (iconName = "ios-add-circle-outline"), (iconType = "ionicon");
+        } else if (routeName === "Lists") {
+          (iconName = "list"), (iconType = "entypo");
+        } else {
+          (iconName = "ios-person"), (iconType = "ionicon");
+        }
+
+        return (
+          <Icon name={iconName} type={iconType} size={28} color={tintColor} />
+        );
+      },
+      tabBarLabel: navigation.state.routeName
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray"
+    }
   }
-});
+);
 
-export default createAppContainer(createRootNavigator);
+export default createAppContainer(TabNavigator);
